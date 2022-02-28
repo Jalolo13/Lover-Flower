51708a1db528331b00bd027f09e6689ade54e30f
Nurai <nooraselfeght@gmail.com>
2022-02-28 22:47:23 +0600

    little touches

--- js/faqscript.js
+++ js/faqscript.js
@@ -2,8 +2,7 @@ let faqBtn = document.querySelectorAll('.question-block__button');
 let faqAnswer = document.querySelectorAll('.question-block__answer');
 
 for(let i =0; i < faqBtn.length; i++){
-  faqBtn[i].addEventListener('click', function(event){
-    let target = EventTarget;
+  faqBtn[i].addEventListener('click', function(e){
     faqBtn[i].classList.toggle('active');
     faqAnswer[i].classList.toggle('open')
   })
